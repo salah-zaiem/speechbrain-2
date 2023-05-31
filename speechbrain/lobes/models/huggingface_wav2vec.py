c9ba90884a5fb74ec158b22d3add5c395c129a3b
salah-zaiem <zaiemsalah@gmail.com>
2023-05-31 13:42:19 +0200

    changed call to huggingface_wav2vec

--- speechbrain/lobes/models/huggingface_wav2vec.py
+++ speechbrain/lobes/models/huggingface_wav2vec.py
@@ -505,35 +505,30 @@ class HuggingFaceWav2Vec2Pretrain(nn.Module):
 
         return src_key_padding_mask
 
+
 class WeightedSSLModel(torch.nn.Module):
-    def __init__(
-        self,
-        hub,
-        num_layers,
-        layernorm = False
-    ):
+    def __init__(self, hub, num_layers, layernorm=False):
         super().__init__()
-        self.encoder = AutoModel.from_pretrained(hub, output_hidden_states = True)
+        self.encoder = AutoModel.from_pretrained(hub, output_hidden_states=True)
         self.num_layers = num_layers
         zero_init = torch.cat([torch.zeros(self.num_layers)])
         self.weights = torch.nn.Parameter(zero_init, requires_grad=True)
         self.layernorm = layernorm
 
     def forward(self, wav, wav_lens=None):
-        feats= self.encoder(wav)
-        hidden_states= torch.stack(feats.hidden_states, dim=0).detach()
-        #First dimension should be equal to the number of layers in the hparams
-        assert self.num_layers == hidden_states.shape[0], "Num layers not equal to num hidden states" 
+        feats = self.encoder(wav)
+        hidden_states = torch.stack(feats.hidden_states, dim=0).detach()
+        # First dimension should be equal to the number of layers in the hparams
+        assert (
+            self.num_layers == hidden_states.shape[0]
+        ), "Num layers not equal to num hidden states"
         norm_weights = torch.nn.functional.softmax(self.weights, dim=-1)
-        if self.layernorm : 
-            hidden_states = [F.layer_norm(t, (t.shape[-1],)) for t in hidden_states]
+        if self.layernorm:
+            hidden_states = [
+                F.layer_norm(t, (t.shape[-1],)) for t in hidden_states
+            ]
         weighted_feats = hidden_states[0] * norm_weights[0]
-        for i in range(1, len(hidden_states)): 
+        for i in range(1, len(hidden_states)):
             weighted_feats += hidden_states[i] * norm_weights[i]
 
         return weighted_feats
-
-
-
-
-
